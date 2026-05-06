b26e0c23fff85beb8be61c715003f8945aa5430a
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2026-05-05 19:45:43 -0600

    Avance: Teoría de la Información
    Primer prototipo terminado de función de recorrido de ventana... No funciona no tengo idea de por qué.

--- Teoría de la información/Tarea Ventana/Ventana.py
+++ Teoría de la información/Tarea Ventana/Ventana.py
@@ -34,26 +34,59 @@ def CrearContador(Tamaño, EspacioPrevio):
 # ==================================== AQUÍ COMIENZA EL FLUJO DEL PROGRAMA ======================================== 
 #Agarramos nuestro texto
 Texto = LeerArchivo()
+DistanciaRestante = len(Texto)
 
 #Agarramos el tamño de ventana
 TamañoVentana = int(input("¿De qué tamaño es la ventana?:  "))
+DistanciaRestante -= TamañoVentana
 
 #Creamos la primer ventana
 Inicio = 0
 Final = TamañoVentana
-Texto_Ventana = Texto[Inicio:Final] 
-Historial = Texto_Ventana[Inicio:(int(len(Texto_Ventana) * 0.7))]
-LookAhead = Texto_Ventana[(int(len(Texto_Ventana) * 0.7)):Final]
 
-#Esta es impresión por motivos de depuración --------------------
-print(f"Para el texto de la ventana: {Texto_Ventana}")
-Contador = CrearContador(TamañoVentana, 28)
-print(Contador, "\n")
-print(f"El historial es: {Historial}")
-Contador = CrearContador(len(Historial), 17)
-print(Contador, "\n")
-print(f"El Lookahead es: {LookAhead}")
-Contador = CrearContador(len(LookAhead), 17)
-print(Contador, "\n")
-# -----------------------------------------------------------------
 
+while DistanciaRestante >= 0:
+    print(f'Quedan {DistanciaRestante} espacios para terminar\n---------------------------------------------------')
+
+    Texto_Ventana = Texto[Inicio:Final] 
+    Historial = Texto_Ventana[Inicio:(int(len(Texto_Ventana) * 0.7))]
+    LookAhead = Texto_Ventana[(int(len(Texto_Ventana) * 0.7)):Final]
+    #Esta es impresión por motivos de depuración --------------------
+    print(f"Para el texto de la ventana: {Texto_Ventana}")
+    Contador = CrearContador(TamañoVentana, 28)
+    print(Contador)
+    print(f"El historial es:{Historial}")
+    Contador = CrearContador(len(Historial), 17)
+    print(Contador)
+    print(f"El Lookahead es:{LookAhead}")
+    Contador = CrearContador(len(LookAhead), 17)
+    print(Contador)
+    # -----------------------------------------------------------------
+
+    LookAheadCorrecto = ''
+    Tamaño = 0
+
+    for Letra in LookAhead:
+        LookAheadCorrecto += Letra
+      
+        if LookAheadCorrecto in Historial:
+            print(f'Termino: {LookAheadCorrecto} encontrado.')
+            Offset = len(Historial) - (Historial.find(Letra))
+            Tamaño += 1
+        else:
+            if LookAheadCorrecto == '':
+                Tamaño = len(LookAhead)
+                Offset = 0
+            print(f'Tamaño = {Tamaño}')
+            print(f'Offset = {Offset}')
+            print(f'Letra que rompió: {Letra}')
+            break
+
+
+
+    
+    Inicio += Tamaño
+    Final += Tamaño
+    DistanciaRestante - Tamaño
+    input()
+            
